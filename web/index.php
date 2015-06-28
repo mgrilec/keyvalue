81faafd3fec9f69d3f58e77a8236e8b08c1f6359
Mihael Grilec <mihael.grilec@gamewerk.com>
2015-06-28 21:30:36 +0200

    Added project create and delete.

--- web/index.php
+++ web/index.php
@@ -1,42 +1,62 @@
 <?php
-	$f3 = require('fatfree/lib/base.php');
-	$f3->set('api', new Api('../api/'));
-	$f3->set('template', Template::instance());
 
-	$f3->route('GET /',
-	    function($f3) {
+$f3 = require('fatfree/lib/base.php');
+$f3->set('api', new Api('../api/'));
+$f3->set('template', Template::instance());
 
-	    	// get all projects
-	    	$projects = $f3->get('api')->projects();
-	    	array_unshift($projects, array('create' => true));
+$f3->route('GET /',
+    function($f3, $params) {
 
-	    	$rows = array();
-	    	for ($i = 0; $i < count($projects); $i++) {
-	    		$row = floor($i / 3);
-	    		$rows[$row][] = $projects[$i];
-	    	}
+    	// get all projects
+    	$projects = $f3->get('api')->projects();
+    	array_unshift($projects, array('create' => true));
 
-	        echo $f3->get('template')->render('projects.html', 'text/html', array('rows' => $rows));
-	    }
-	);
+    	$rows = array();
+    	for ($i = 0; $i < count($projects); $i++) {
+    		$row = floor($i / 3);
+    		$rows[$row][] = $projects[$i];
+    	}
 
-	$f3->route('GET /@project_id',
-	    function($f3, $params) {
+        echo $f3->get('template')->render('projects.html', 'text/html', array('rows' => $rows));
+    }
+);
 
-	    	// get params
-	    	$project_id = $params['project_id'];
+$f3->route('POST /create', 
+	function($f3, $params) {
+		// create new project
+		$response = $f3->get('api')->project_create($f3->get('POST.project_title'));
+		$id = $response['id'];
 
-	    	// check project id
-	    	if (ctype_digit($project_id)) {
-	    		$project_id = intval($project_id);
-	    	}
-	    	else {
-	    		$f3->reroute('/');
-    		}
+		$f3->reroute('/');
+	}
+);
 
-	        echo $f3->get('template')->render('edit.html');
-	    }
-	);
+$f3->route('GET /@project_id/delete',
+	function($f3, $params) {
+		// delete project
+		$f3->get('api')->project_delete($params['project_id']);
+		$f3->reroute('/');
+	}
+);
 
-	$f3->run();
+$f3->route('GET /@project_id',
+    function($f3, $params) {
+
+    	// get params
+    	$project_id = $params['project_id'];
+
+    	// check project id
+    	if (ctype_digit($project_id)) {
+    		$project_id = intval($project_id);
+    	}
+    	else {
+    		$f3->reroute('/');
+		}
+
+        echo $f3->get('template')->render('edit.html');
+    }
+);
+
+$f3->run();
+
 ?>
