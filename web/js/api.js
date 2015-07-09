2519eb09b04e138cfd7745c090e7b67c60368a6f
Mihael Grilec <mihael.grilec@gamewerk.com>
2015-07-09 19:09:40 +0200

    Started api.js.

---
+++ web/js/api.js
@@ -0,0 +1,21 @@
+var api = (function($) {
+
+	var api = {};
+	api.url = '../api/';
+
+	var get = function(route, success, fail) {
+		var url = api.url + route;
+		return $.get(url, success, 'json').fail(fail);
+	};
+
+	var post = function(route, params, success, fail) {
+		var url = this.url + route;
+		return $.post(url, params, success, 'json').fail(fail);
+	};
+
+	api.projects = function(success) {
+		return get('projects', success);
+	};
+
+	return api;
+})(jQuery);
--- web/js/projects.js
+++ web/js/projects.js
@@ -1,7 +1,17 @@
   $(document).ready(function(){
+
+  	// masonry
     $('#cards .row').masonry({
       itemSelector: '#cards .col',
     });
+
+    // modal
     $('.modal-trigger').leanModal();
+
+    // side nav?
     $('.button-collapse').sideNav();
+
+    api.projects(function(data) {
+		console.log(data);
+    });
   });
