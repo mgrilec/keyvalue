538a58e02497a1a1ab6bc0e600fa3478fc33e075
Mihael Grilec <mihael.grilec@gamewerk.com>
2015-07-10 01:08:57 +0200

    Project adding through ajax.

--- web/js/api.js
+++ web/js/api.js
@@ -14,19 +14,27 @@ var api = (function($) {
 	};
 
 	api.projects = function(success) {
-		return get('projects', function(data) { success(data['result']); });
+		return get('projects', function(data) { success(data.result); });
+	};
+
+	api.project_create = function(project_title, project_description, success) {
+		return post('projects/create', { project: { title: project_title, description: project_description }}, function(data) { success(data.result); })
+	};
+
+	api.project_get = function(project_id, success) {
+		return get('projects/' + project_id, function(data) { success(data.result); });
 	};
 
 	api.project_delete = function(project_id, success) {
-		return post('projects/delete', { project: { id: project_id }}, function(data) { success(data['result'])});
+		return post('projects/delete', { project: { id: project_id }}, function(data) { success(data.result); });
 	};
 
 	api.keys_count = function(project_id, success) {
-		return get('keys/' + project_id + "/count", function(data) { success(data['result']);})
+		return get('keys/' + project_id + "/count", function(data) { success(data.result); })
 	};
 
 	api.keys_get_all = function(project_id, success) {
-		return get('keys/' + project_id, function(data) { success(data['result']);})
+		return get('keys/' + project_id, function(data) { success(data.result); })
 	};
 
 	return api;
--- web/js/projects.js
+++ web/js/projects.js
@@ -1,7 +1,7 @@
-  $(document).ready(function(){
+$(document).ready(function(){
 
   	// masonry
-    $('#projects').masonry({
+    var grid = $('#projects').masonry({
       itemSelector: '#project',
     });
 
@@ -11,6 +11,32 @@
     // side nav?
     $('.button-collapse').sideNav();
 
+    var add = function(card) {
+		// on delete
+		card.find('.delete').click(function(e) {
+			api.project_delete(e.target.id, function(result) {
+				if (result) {
+					remove(card);
+					Materialize.toast('Project deleted.', 4000);
+				} else {
+					Materialize.toast('Unable to delete project.', 4000);
+				}
+			});
+			
+		});
+
+		grid.prepend(card);
+		grid.masonry('prepended', card);
+		grid.masonry();
+    };
+
+    var remove = function(card) {
+    	card.remove();
+    	grid.masonry('remove', card);
+    	grid.masonry();
+    }
+
+    // get all projects
     async.series({
     	// compile template
     	template: function(callback) {
@@ -18,6 +44,7 @@
     			callback(null, Handlebars.compile(data)); 
     		}, 'html');
     	},
+    	// get projects
     	projects: function(projectsCallback) {
     		api.projects(function(projects) {
 
@@ -37,20 +64,43 @@
 	function(error, results) {
 		results.projects.forEach(function(project, index) {
 				var card = $(results.template({ project: project }));
-
-				card.find('.delete').click(function(e) {
-					api.project_delete(e.target.id, function(result) {
-						if (result) {
-							card.remove();
-							Materialize.toast('Project deleted.', 4000)
-						} else {
-							Materialize.toast('Unable to delete project.', 4000)
-						}
-					});
-					
-				});
-
-				$('#projects').append(card);
+				add(card);
 			});
 	});
-  });
+
+	// create new project
+	$('#create').submit(function(e) {
+		var formData = $(this).serializeArray();
+		var data = {};
+		formData.forEach(function(item) {
+			data[item.name] = item.value;
+		});
+
+		api.project_create(data['project_title'], data['project_create'], function(id) {
+
+			// add a project card
+			async.parallel({
+				template: function(callback) {
+					$.get('ui/projects/project-card.html', function(data) { 
+		    			callback(null, Handlebars.compile(data)); 
+		    		}, 'html');
+				},
+				project: function(callback) {
+					api.project_get(id, function(project) {
+						callback(null, project);
+					});
+				}
+			}, function(error, results) {
+				var card = $(results.template({ project: results.project }));
+				add(card);
+				Materialize.toast('Project created.', 4000);
+			});
+		});
+
+		// reset form fields
+		$(this).trigger('reset');
+
+		// prevent form going off
+		e.preventDefault();
+	});
+});
